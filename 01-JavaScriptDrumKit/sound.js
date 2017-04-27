0767ca37ad368a70216edd9816df6ac3437a030a
Chang <tanteichang@outlook.com>
2017-04-28 01:49:50 +0800

    优化代码结构

--- 01-JavaScriptDrumKit/sound.js
+++ 01-JavaScriptDrumKit/sound.js
@@ -1,22 +1,20 @@
 (function () {
 
-  function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }
-
   function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
 
     key.classList.add('playing');
-    audio.currentTime = 0;
+    audio.currentTime = 0; // 设置播放时间戳为 0 保证连续响起鼓声
     audio.play();
   }
-
+  function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
   const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition)); // 在 CSS transition 事件结束后除去样式
 
   window.addEventListener('keydown', playSound);
 })();
